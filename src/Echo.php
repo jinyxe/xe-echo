9cf870e68bf3647396117c9503083153df218ed7
hojinlee <infohojin@gmail.com>
2018-10-29 17:18:40 +0900

    init

---
+++ src/Echo.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of the XE package.
+ *
+ * (c) hojinlee <infohojin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace JinyXE\XE_echo;
+
+class Trans
+{
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+}
